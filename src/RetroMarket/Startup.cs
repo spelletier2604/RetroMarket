e406ade0b0d907648c78398188df2d856dae2c7e
TheMasterZelda <davidfoulernier@hotmail.ca>
2017-04-08 13:57:00 -0400

    Ajout Langues
    Ajouts des sélections de langues et de la sauvegarde de session pour
    cette sélection. Texte traduit a venir.

--- src/RetroMarket/Startup.cs
+++ src/RetroMarket/Startup.cs
@@ -6,49 +6,78 @@ using Microsoft.Extensions.Logging;
 using RetroMarket.Models;
 using Microsoft.Extensions.Configuration;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Razor;
+using System.Globalization;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.Extensions.Options;
 
-namespace RetroMarket {
+namespace RetroMarket
+{
 
-    public class Startup {
+    public class Startup
+    {
         IConfigurationRoot Configuration;
 
-        public Startup(IHostingEnvironment env) {
+        public Startup(IHostingEnvironment env)
+        {
             Configuration = new ConfigurationBuilder()
                 .SetBasePath(env.ContentRootPath)
                 .AddJsonFile("appsettings.json").Build();
         }
 
-public void ConfigureServices(IServiceCollection services) {
-    services.AddDbContext<ApplicationDbContext>(options =>
-        options.UseSqlServer(
-            Configuration["Data:SportStoreProducts:ConnectionString"]));
-    services.AddTransient<IProductRepository, EFProductRepository>();
-    services.AddScoped<Cart>(sp => SessionCart.GetCart(sp));
-    services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
-    services.AddTransient<IOrderRepository, EFOrderRepository>();
-    services.AddMvc();
-    services.AddMemoryCache();
-    services.AddSession();
-}
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(
+                    Configuration["Data:SportStoreProducts:ConnectionString"]));
+            services.AddTransient<IProductRepository, EFProductRepository>();
+            services.AddScoped<Cart>(sp => SessionCart.GetCart(sp));
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddTransient<IOrderRepository, EFOrderRepository>();
+            services.AddLocalization(options => options.ResourcesPath = "Resources");
+            services.AddMvc()
+                    .AddViewLocalization(LanguageViewLocationExpanderFormat.Suffix)
+                    .AddDataAnnotationsLocalization();
+            services.Configure<RequestLocalizationOptions>(
+                options =>
+                {
+                    var supportedCultures = new[] {
+                        new CultureInfo("en-CA"),
+                        new CultureInfo("fr-CA")
+                    };
+
+                    options.DefaultRequestCulture =
+                        new RequestCulture(culture: "fr-CA", uiCulture: "fr-CA");
+                    options.SupportedCultures = supportedCultures;
+                    options.SupportedUICultures = supportedCultures;
+                });
+            services.AddMemoryCache();
+            services.AddSession();
+        }
 
         public void Configure(IApplicationBuilder app,
-                IHostingEnvironment env, ILoggerFactory loggerFactory) {
+                IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
 
 
-            app.UseRequestLocalization(new RequestLocalizationOptions {
+            app.UseRequestLocalization(new RequestLocalizationOptions
+            {
                 DefaultRequestCulture = new Microsoft.AspNetCore.Localization.RequestCulture("en-US")
             });
 
+            var locOptions = app.ApplicationServices.GetService<IOptions<RequestLocalizationOptions>>();
+            app.UseRequestLocalization(locOptions.Value);
 
             app.UseDeveloperExceptionPage();
             app.UseStatusCodePages();
             app.UseStaticFiles();
             app.UseSession();
-            app.UseMvc(routes => {
+            app.UseMvc(routes =>
+            {
                 routes.MapRoute(
                         name: "Default",
-                        template:"Home",
-                        defaults: new {controller = "Home", action = "Index"}
+                        template: "Home",
+                        defaults: new { controller = "Home", action = "Index" }
                     );
 
                 routes.MapRoute(
